5517893e4e3432075dc3886b013f1df5d55f28da
joe <baymax_studio@foxmail.com>
2016-04-12 21:50:44 +0800

    add viewpager sample

---
+++ viewpager/src/main/java/com/raptors/viewpager/ViewPagerActivity.java
@@ -0,0 +1,14 @@
+package com.raptors.viewpager;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ViewPagerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_pager);
+        setTitle("View Pager");
+    }
+}
